0ec70396749726ed639c637efdbe4677fa1d49dd
KFad <kfadigan@yahoo.com>
2019-04-08 10:13:02 -0400

    rename files & establish load dependencies

--- tournaments/lib/tournaments.rb
+++ tournaments/lib/tournaments.rb
@@ -1,6 +1,5 @@
-require "tournaments/version"
+require_relative "./tournaments/version"
+require_relative './tournaments/cli'
+#this acts as the environments, loading the other files.
 
-module Tournaments
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
---
+++ tournaments/lib/tournaments/cli.rb
@@ -0,0 +1,8 @@
+#this is the controller
+class Tournaments::CLI
+
+  def call
+    puts "Upcoming Tournaments:"
+  end
+
+end
