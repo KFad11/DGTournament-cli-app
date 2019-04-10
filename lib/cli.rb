f00829e615d066f83a07043e59ce7c17b8b4b9b9
KFad <kfadigan@yahoo.com>
2019-04-10 10:24:10 -0400

    Stub out CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,10 +2,36 @@
 class CLI
 
   def call
-    puts "Welcome to Pennsylvania Tournament Tracker"
-    puts "Please select 'C' for a list of C Tiers, or 'B' for B Tiers:"
-    EventScraper.make_c_tournaments
-    # puts "#{tourney.name}"
+    welcome
+    menu
+    goodbye
   end
 
+  def welcome
+    puts "Welcome to Pennsylvania Disc Golf Tournament Tracker"
+    puts "Here we can see what disc golf tournaments are happening, when, and what competition level they are based on their 'tier'"
+    puts "B Tier tournaments are higher than C Tier in terms of competition"
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Please select 'C' for a list of C Tiers, or 'B' for B Tiers, or type exit:"
+      input = gets.strip.downcase
+      case input
+      when "c"
+        puts "C Tier list"
+      when "b"
+        puts "B Tier list"
+      else
+        puts "Not a valid tier to search, type either the tier letters"
+      end
+    end
+  end
+
+  def goodbye
+    puts "Come back soon to check up on the tournaments happening in PA!"
+  end
 end
+
+#EventScraper.make_c_tournaments
--- lib/event_scraper.rb
+++ lib/event_scraper.rb
@@ -5,15 +5,18 @@ class EventScraper
     Nokogiri::HTML(open("https://www.discgolfscene.com/tournaments/Pennsylvania"))
   end
 
-  def self.get_tounaments
+  def self.get_tournaments
     self.get_page.css(".tournaments-listing-all")
   end
 
   def self.make_c_tournaments
-    self.get_tounaments.each do |tournaments|
+    self.get_tournaments.each do |tournaments|
       tournaments.css(".tournament-C").map do |ctier|
-        tourney = Tournament.new
-        tourney.name = ctier.css("em").text
+        # binding.pry
+        tourneyC = Tournament.new
+        tourneyC.name = ctier.css("em").text
+        tourneyC.date = ctier.css(".t-date").text
+        tourneyC.tier = ctier.css(".info.ts").text
       end
     end
   end
